fd769b6a516d9e9712c4c7cd0271808a8e0ab9a8
Christopher Quezada <cpqm07@gmail.com>
2018-08-21 20:23:42 -0300

    fourth commit

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 2018_08_21_225559) do
   create_table "students", force: :cascade do |t|
     t.string "name"
     t.integer "age"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
